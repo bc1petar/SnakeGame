ffc465e532dadf2c6e2d32ce7784de84934f0943
Petar <34079554+ptrsploit@users.noreply.github.com>
2018-01-14 21:47:58 +0100

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,20 @@
+package snake;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Snake {
+
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Snake game");
+        frame.setContentPane(new GamePanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        
+        frame.setPreferredSize(new Dimension(GamePanel.WIDTH,GamePanel.HEIGHT));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }  
+}
